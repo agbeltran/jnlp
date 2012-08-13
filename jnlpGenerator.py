4757a549d6834615febb8f134fe6fa2369a16af7
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-08-13 17:57:44 +0100

    Testing cgi.FieldStorage()

--- jnlpGenerator.py
+++ jnlpGenerator.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+# Import the CGI module
+import cgi
+
 # enable debugging
 # TODO remove in production version
 import cgitb
 cgitb.enable()
 
-print "Content-Type: text/plain;charset=utf-8"
-print
 
-print "Hello World!"
+def main():
+    #print "Content-Type: text/plain;charset=utf-8"
+    #print
+    #print "Hello World!"
+    fs = cgi.FieldStorage()
+    print "Content-type: text/plain\n\n"
+    for key in fs.keys():
+        print "%s = %s" % (key, fs[key].value)
 
+
+main()
