a929fc4fec741e0f8bff01ec68adc3fa3bfabbb7
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-08-13 16:36:56 +0100

    First version of script - testing cgi

---
+++ jnlpGenerator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+# TODO remove in production version
+import cgitb
+cgitb.enable()
+
+print "Content-Type: text/plain;charset=utf-8"
+print
+
+print "Hello World!"
+
