0553541426f856fc17e0517c6ab7cc1fa79ce236
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-09-24 13:00:55 +0100

    Adding template file

--- jnlpGenerator.py
+++ jnlpGenerator.py
@@ -12,40 +12,40 @@ from string import Template
 import cgitb
 cgitb.enable()
 
-JNLP_TEMPLATE = """
-<?xml version="1.0" encoding="utf-8"?>
+import os.path
+
+JNLP_TEMPLATE = """<?xml version="1.0" encoding="utf-8"?>
 
 <!DOCTYPE jnlp PUBLIC "-//Sun Microsystems, Inc//DTD JNLP Descriptor 6.0//EN" "http://java.sun.com/dtd/JNLP-6.0.dtd">
 <!-- doctype points to Sun, not Oracle for compatibility. -->
-<jnlp spec="6.0+" codebase="http://isatab.sourceforge.net/jnlp/" href="ISAcreator.jnlp" version="1.6">
+<jnlp spec="6.0+" codebase=$codebase href=$href version="1.6">
 
-	<information>
- 		<title>ISAcreator 1.6</title>
- 		<vendor>ISA team, Oxford e-Research Centre, University of Oxford, Oxford, UK</vendor>
- 		<homepage href="http://isa-tools.org" />
- 		<description>ISAcreator tool</description>
- 		<version>1.6</version>
- 		<icon href="http://isatab.sourceforge.net/jnlp/isacreator_logo.png"/>
- 		 <shortcut online="true">
-     		 <desktop />
-    		  <menu submenu="ISAcreator" />
-   		 </shortcut>
- 		<offline-allowed/>
- 	</information>
+    <information>
+        <title>$title</title>
+        <vendor>ISA team, Oxford e-Research Centre, University of Oxford, Oxford, UK</vendor>
+        <homepage href="http://isa-tools.org" />
+        <description>$description</description>
+        <version>1.6</version>
+        <icon href=$icon/>
+        <shortcut online="true">
+            <desktop />
+            <menu submenu=$submenu />
+        </shortcut>
+        <offline-allowed/>
+    </information>
 
- 	<security>
- 		<all-permissions/>
- 	</security>
+    <security>
+        <all-permissions/>
+    </security>
 
- 	<update check="always" policy="always" />
+    <update check="always" policy="always" />
 
- 	<resources>
- 		<j2se version="1.5+" />
- 		<jar href="http://isatab.sourceforge.net/jnlp/ISAcreator-1.6.jar" />
- 	</resources>
-	<application-desc main-class="org.isatools.isacreator.gui.modeselection.ModeSelector"/>
-	$arguments
- </jnlp>
+    <resources>
+        <j2se version="1.5+" />
+        <jar href=$jar />
+    </resources>
+    <application-desc main-class=$mainclass/>
+</jnlp>
 """
 
 #import cherrypy
@@ -58,16 +58,12 @@ JNLP_TEMPLATE = """
 
 #@cherrypy.expose
 #def index(self):
-#    tmpl = loader.load('ISAcreator.jnlp')
+#    tmpl = loader.load('template.jnlp')
 #    fs = cgi.FieldStorage()
 #    return tmpl.generate(title='ISAcreator 1.6',description='ISAcreator tool',submenu='ISAcreator',jar='http://isatab.sourceforge.net/jnlp/ISAcreator-1.6.jar').render('html', doctype='html')
 
 
 def main():
-    #print "Content-Type: text/plain;charset=utf-8"
-    #print
-    #print "Hello World!"
-
     from string import Template
 
     #### parse arguments
@@ -76,11 +72,38 @@ def main():
 
     for key in fs.keys():
         args =  args+"<argument>--"+key+"</argument>\n <argument>"+fs[key].value+"</argument>"
+
+    codebase = '"http://isatab.sourceforge.net/jnlp/"'
+    href = '"template.jnlp"'
+    title = '"ISAcreator 1.7"'
+    vendor = '"ISA team, Oxford e-Research Centre, University of Oxford, Oxford, UK"'
+    homepage = '"http://isa-tools.org"'
+    description = '"ISAcreator tool"'
+    version = '"1.7"'
+    icon = '"http://isatab.sourceforge.net/jnlp/isacreator_logo.png"'
+    submenu = '"ISAcreator"'
+    j2se_version = '"1.5+"'
+    jar = '"http://toad.oerc.ox.ac.uk:8080/ISAcreator/ISAcreator-1.6.jar"'
+    mainclass = '"org.isatools.isacreator.launch.ISAcreatorApplication"'
+
+
+
+
     jnlp = Template(JNLP_TEMPLATE)
-    jnlp_file = open("/tmp/ISAcreator-parameters.jnlp", "w")
-    jnlp_file.write(jnlp.substitute(arguments=args))
-    jnlp_file.close()
+    jnlp = jnlp.substitute(title=title,
+                           vendor=vendor,
+                           description=description,
+                           icon=icon,
+                           submenu=submenu,
+                           mainclass=mainclass,
+                           codebase=codebase,
+                           href=href,
+                           jar=jar,
+                           arguments=args)
 
+    filename = "template.jnlp"
 
+    print "Content-Type:application/x-download\nContent-Disposition:attachment;filename=%s\n\n%s" % (filename,jnlp)
+    #print "Content-Length:%s\n\n%s" %   jnlp, len(jnlp), jnlp#(os.path.split(jnlp)[-1], len(buff), buff)
 
 main()
