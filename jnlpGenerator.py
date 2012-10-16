d6d1dbc4e9a31eccc3c373d2054d31a2f13ce5e7
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2012-10-16 23:39:38 +0100

    Changes in template and script

--- jnlpGenerator.py
+++ jnlpGenerator.py
@@ -25,30 +25,31 @@ def main():
     args = ""
 
     for key in fs.keys():
-        args =  args+"<argument>--"+key+"</argument>\n <argument>"+fs[key].value+"</argument>"
+        args =  args+"\t<argument>--"+key+"</argument>\n\t<argument>"+fs[key].value+"</argument>"
 
     codebase = '"http://toad.oerc.ox.ac.uk:8080/ISAcreator/"'
-    href = '"ISAcreator.jnlp"'
-    title = '"ISAcreator 1.7"'
+    href = '""'
+    title = '"ISAcreator 1.7.0"'
     vendor = '"ISA team, Oxford e-Research Centre, University of Oxford, Oxford, UK"'
     homepage = '"http://isa-tools.org"'
     description = '"ISAcreator tool"'
-    version = '"1.7"'
+    version = '"1.7.0"'
     icon = '"http://isatab.sourceforge.net/jnlp/isacreator_logo.png"'
     submenu = '"ISAcreator"'
     j2se_version = '"1.5+"'
-    jar = '"http://toad.oerc.ox.ac.uk:8080/ISAcreator/ISAcreator-1.6.jar"'
+    jar = '"http://toad.oerc.ox.ac.uk:8080/ISAcreator/ISAcreator-1.7.0.jar"'
     mainclass = '"org.isatools.isacreator.launch.ISAcreatorApplication"'
 
+    filename = "ISAcreator-1.7.0.jnlp"
 
     jnlp = Template(JNLP_TEMPLATE)
     jnlp = jnlp.substitute(codebase=codebase,
                            href=href,
+                           version=version,
                            title=title,
                            vendor=vendor,
                            homepage=homepage,
                            description=description,
-                           version=version,
                            icon=icon,
                            submenu=submenu,
                            j2se_version=j2se_version,
@@ -56,7 +57,6 @@ def main():
                            jar=jar,
                            arguments=args)
 
-    filename = "ISAcreator.jnlp"
 
     print "Content-Type:application/x-download\nContent-Disposition:attachment;filename=%s\n\n%s" % (filename,jnlp)
 
